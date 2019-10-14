8961608bdbd308f86e5e9dcf943411203de0309f
Ethan <ethanling1@gmail.com>
2019-10-13 20:31:54 -0400

    Updates

---
+++ precache-manifest.63c104b40536af417a54245dcf8367a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e05bc84413dfdf3793c6281ed8d5ec",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "6fc94243932ee1f3b9f1",
+    "url": "/todo/static/css/main.317e2ffa.chunk.css"
+  },
+  {
+    "revision": "ba98722f80a9017b1bd8",
+    "url": "/todo/static/js/2.ed729d8c.chunk.js"
+  },
+  {
+    "revision": "6fc94243932ee1f3b9f1",
+    "url": "/todo/static/js/main.343a36fd.chunk.js"
+  },
+  {
+    "revision": "aac1db0e225ec5e340d3",
+    "url": "/todo/static/js/runtime-main.24ce7195.js"
+  },
+  {
+    "revision": "667d82e4af763edb42bd33580d80f0c1",
+    "url": "/todo/static/media/delete.667d82e4.svg"
+  }
+]);
